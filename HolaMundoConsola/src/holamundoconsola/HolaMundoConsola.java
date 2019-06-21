d1dcd568c9d69b38eb76d0207f468cc21d32053b
Laboratorios <Laboratorios@Q05>
2019-06-20 20:02:13 -0600

    hola mundo hecho en clase en consola en el main

---
+++ HolaMundoConsola/src/holamundoconsola/HolaMundoConsola.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoconsola;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class HolaMundoConsola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+              System.out.println("hi");
+             String pez= JOptionPane.showInputDialog("");
+        
+    }   
+
+    
+    
+}
